f3a88d17356f50952744cdb4aa4a09d4030f58f4
mezabyte <51653293+mezabyte@users.noreply.github.com>
2023-11-12 15:41:56 +0100

    base64

--- src/Verification/Stage3.tsx
+++ src/Verification/Stage3.tsx
@@ -38,7 +38,7 @@ export default function Stage3() {
                 localStorage.getItem('tk_robloxr'),
                 localStorage.getItem('tk_discord')
             )
-            const url = import.meta.env.VITE_APIURL+"verification/finish?rbx="+localStorage.getItem('tk_robloxr')!+"&dsc="+localStorage.getItem('tk_discord')!
+            const url = import.meta.env.VITE_APIURL+"verification/finish?rbx="+btoa(localStorage.getItem('tk_robloxr')!)+"&dsc="+btoa(localStorage.getItem('tk_discord')!)
             const f0 = await ApiGet(url)
             if (!f0.ok) {
                 if (f0.status !== 400)
